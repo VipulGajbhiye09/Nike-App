589fb29e7c8df0d501583e0e31eeda881bb9ced5
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-10-26 22:44:31 +0530

    Update SpecialOffer.jsx

--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -3,7 +3,6 @@ import { offer } from "../assets/images";
 const SpecialOffer = () => {
   return (
     <section className="flex justify-between items-center max-xl:flex-col-reverse gap-10 max-container">
-      
       <div className="flex-1">
         <img
           src={offer}
@@ -13,7 +12,22 @@ const SpecialOffer = () => {
           className="object-contain w-full"
         />
       </div>
-      
+      <div className="flex flex-1 flex-col">
+        <h2 className="text-4xl font-palanquin font-bold">
+          <span className="text-coral-red">Special </span>
+          Offer
+        </h2>
+        <p className="mt-4 info-text">
+          Embark on a shopping journey that redefines your experience with
+          unbeatable deals. From premier selections to incredible savings, we
+          offer unparalleled value that sets us apart.
+        </p>
+        <p className="mt-6 info-text">
+          Navigate a realm of possibilities designed to fulfill your unique
+          desires, surpassing the loftiest expectations. Your journey with us is
+          nothing short of exceptional.
+        </p>
+
     </section>
   );
 };
