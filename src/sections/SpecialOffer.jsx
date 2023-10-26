61f0f2f638c3a8d9ad1d85ff42f01ac1d8ae5373
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-10-26 22:43:52 +0530

    Create SpecialOffer.jsx

---
+++ src/sections/SpecialOffer.jsx
@@ -0,0 +1,21 @@
+import { offer } from "../assets/images";
+
+const SpecialOffer = () => {
+  return (
+    <section className="flex justify-between items-center max-xl:flex-col-reverse gap-10 max-container">
+      
+      <div className="flex-1">
+        <img
+          src={offer}
+          alt="Shoe Promotion"
+          width={773}
+          height={687}
+          className="object-contain w-full"
+        />
+      </div>
+      
+    </section>
+  );
+};
+
+export default SpecialOffer;
