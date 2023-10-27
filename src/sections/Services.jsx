c7bbca078c6f3c32fad349f035197b0130f32ec5
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-10-27 23:04:17 +0530

    Update Services.jsx

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,11 +1,12 @@
+import { services } from "../constants";
 import  ServiceCard  from "../components/ServiceCard";
 
 const Services = () => {
   return (
     <section className="max-container flex justify-center flex-wrap gap-9">
-      <div>
-        Sample Service Div
-      </div>
+      {services.map((service) => (
+        <ServiceCard key={service.label} {...service} />
+      ))}
     </section>
   );
 };
