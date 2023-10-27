dbf07ffc6325303d61569f3d92cfea33e7468471
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-10-27 23:04:02 +0530

    Create Services.jsx

---
+++ src/sections/Services.jsx
@@ -0,0 +1,13 @@
+import  ServiceCard  from "../components/ServiceCard";
+
+const Services = () => {
+  return (
+    <section className="max-container flex justify-center flex-wrap gap-9">
+      <div>
+        Sample Service Div
+      </div>
+    </section>
+  );
+};
+
+export default Services;
