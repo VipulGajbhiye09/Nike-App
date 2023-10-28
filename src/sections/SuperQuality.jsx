efab562346e3dac0bd88c261c7a0a39655ae7a19
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-10-28 21:40:38 +0530

    Update SuperQuality.jsx

--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -12,9 +12,18 @@ const SuperQuality = () => {
           <span className="text-coral-red"> Super </span>
           <span className="text-coral-red">Quality </span> Shoes
         </h2>
-        
+        <p className="mt-4 lg:max-w-lg info-text">
+          Ensuring premium comfort and style, our meticulously crafted footwear
+          is designed to elevate your experience, providing you with unmatched
+          quality, innovation, and a touch of elegance.
+        </p>
+        <p className="mt-6 lg:max-w-lg info-text">
+          Our dedication to detail and excellence ensures your satisfaction
+        </p>
+        <div className="mt-11">
+          <Button label="View details" />
+        </div>
       </div>
-
     </section>
   );
 };
