8d4caeb85a575d0d119616ff0d89d572f974dac6
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-10-28 21:40:18 +0530

    Create SuperQuality.jsx

---
+++ src/sections/SuperQuality.jsx
@@ -0,0 +1,22 @@
+import  Button  from "../components/Button";
+
+const SuperQuality = () => {
+  return (
+    <section
+      id="about-us"
+      className="flex justify-between items-center max-lg:flex-col gap-10 w-full max-container"
+    >
+      <div className="flex flex-1 flex-col">
+        <h2 className="font-palanquin capitalize text-4xl lg:max-w-lg font-bold">
+          We Provide You
+          <span className="text-coral-red"> Super </span>
+          <span className="text-coral-red">Quality </span> Shoes
+        </h2>
+        
+      </div>
+
+    </section>
+  );
+};
+
+export default SuperQuality;
