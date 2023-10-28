e11b004e36aeb6853c29374ef8dc88967386da53
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-10-28 21:40:45 +0530

    Update SuperQuality.jsx

--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,3 +1,4 @@
+import { shoe8 } from "../assets/images";
 import  Button  from "../components/Button";
 
 const SuperQuality = () => {
@@ -23,6 +24,16 @@ const SuperQuality = () => {
         <div className="mt-11">
           <Button label="View details" />
         </div>
+      </div>
+
+      <div className="flex-1 flex justify-center items-center">
+        <img
+          src={shoe8}
+          alt="product detail"
+          width={570}
+          height={522}
+          className="object-contain"
+        />
       </div>
     </section>
   );
