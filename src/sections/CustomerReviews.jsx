82734191417d65bbbc9118e7658c714edd1aee7c
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-10-30 22:39:46 +0530

    Update CustomerReviews.jsx

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -1,3 +1,6 @@
+import  ReviewCard from "../components/ReviewCard";
+import { reviews } from "../constants";
+
 const CustomerReviews = () => {
   return (
     <section className="max-container">
@@ -10,6 +13,18 @@ const CustomerReviews = () => {
         Hear genuine stories from our satisfied customers about their
         exceptional experiences with us.
       </p>
+
+      <div className="mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14">
+        {reviews.map((review, index) => (
+          <ReviewCard
+            key={index}
+            imgURL={review.imgURL}
+            customerName={review.customerName}
+            rating={review.rating}
+            feedback={review.feedback}
+          />
+        ))}
+      </div>
     </section>
   );
 };
